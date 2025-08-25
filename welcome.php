f982f409e222a6b84ffe8288517262e31cc9661b
marlmunar <marlemmmunar@gmail.com>
2025-08-25 19:23:03 +0800

    Made my first DB connection via PHP

--- welcome.php
+++ welcome.php
@@ -1,4 +1,17 @@
 <?php
+// mysqli::__construct(
+//     string $host = "localhost",
+//     string $username = "root",
+//     string $password = "",
+//     string $database = "",
+//     int $port = 3306,
+//     string $socket = ""
+// )
+
+$conn = new mysqli('localhost', 'root', '', 'php_site');
+if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
+
 $name = $_POST['username'] ?? 'Guest';
+$conn->query("INSERT INTO users (username) VALUES ('$name')");
 echo "<h1>Welcome, $name!</h1>";
 ?>
