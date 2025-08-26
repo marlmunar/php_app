895a45644b8539d3410745cf83f39fae70cce7cf
marlmunar <marlemmmunar@gmail.com>
2025-08-26 11:10:21 +0800

    Created a login form

---
+++ login.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <h2>Login</h2>
+    <form method="POST" action="authenticate.php">
+        Username: <input type="text" name="username" required><br>
+        Password: <input type="password" name="password" required><br>
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
